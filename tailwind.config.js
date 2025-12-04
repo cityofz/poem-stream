6ad7dbfe0ddccc9fa68514f0e11330e563c8bdfd
cityofz <cityofzach@gmail.com>
2025-12-03 21:27:53 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        mono: ['"JetBrains Mono"', 'monospace'],
-      },
-    },
-  },
-  plugins: [],
-}
+   export default {
+     content: [
+       "./index.html",
+       "./**/*.{js,ts,jsx,tsx}",
+     ],
+     theme: {
+       extend: {},
+     },
+     plugins: [],
+   }
